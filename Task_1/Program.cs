ff7f5d0d0dbfd7698f3faa7303de914837974ea6
Maria <vilochka-17@yandex.ru>
2022-10-29 21:06:12 +0300

    First Task_1 made+Readme

---
+++ Task_1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Hello, my Freind! Please enter the three-digit number: ");
+int number = Int32.Parse(Console.ReadLine());
+int a = number / 100; 
+int b = (number / 10) % 10;
+int c = number % 10;
+Console.WriteLine(b);
